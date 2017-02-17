d0a5dc1e24924e1b373137741921f4df1bbd205c
pd222dj <pd222dj@student.lnu.se>
2017-02-17 15:12:59 +0100

    added HTTP-server. lame tests with request/response

---
+++ src/HTTPServer.java
@@ -0,0 +1,195 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+/**
+ * Simple multi-threaded HTTP-Echo server
+ * @author Peter Danielsson, pd222dj@student.lnu.se
+ */
+public class HTTPServer
+{
+    public static final int BUFSIZE = 1024;
+    public static final int MYPORT = 8888;
+    public static final int MS_PER_SECOND = 1000;
+    public static final int CONNECTION_TIME_OUT = 2 * MS_PER_SECOND; // Connection timeout in ms
+
+    public static void main(String[] args) {
+
+        // Print out message at startup
+        System.out.printf("HTTP-Server running on port %d ...\n", MYPORT);
+
+        try
+        {
+            // Create a socket for communication
+            ServerSocket socket = new ServerSocket(MYPORT);
+
+            // Endless main-loop
+            while (true)
+            {
+                // Wait for a client to connect
+                Socket clientSocket = socket.accept();
+
+                // Create a new thread for handling the connection
+                Thread clientConnection = new Thread(new ClientConnectionThread(clientSocket, BUFSIZE, CONNECTION_TIME_OUT));
+                clientConnection.start();
+            }
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
+
+/**
+ * A Client Connection handler-class that is used by the HTTP-Server
+ * to create Threads for each Client connection
+ * @author Peter Danielsson, pd222dj@student.lnu.se
+ */
+class ClientConnectionThread implements Runnable
+{
+    private Socket clientSocket;
+    private int buffSize;
+    private int connectionTimeOut;
+
+    public ClientConnectionThread(Socket socket, int buffSize, int connectionTimeOut)
+    {
+        this.clientSocket = socket;
+        this.buffSize = buffSize;
+        this.connectionTimeOut = connectionTimeOut;
+    }
+
+    @Override
+    public void run()
+    {
+        try
+        {
+            // Get raw byte-streams for Input/Output
+            InputStream in = clientSocket.getInputStream();
+            OutputStream out = clientSocket.getOutputStream();
+
+            boolean done = false;
+
+            // Will automatically close connection after a time-out period
+            while (!done)
+            {
+                // Start clock
+                long start = System.currentTimeMillis();
+
+                // Used as a temporary storage when receiving data
+                ByteArrayOutputStream temp = new ByteArrayOutputStream();
+
+                // Wait for a message or until time out (client has probably closed connection)
+                while(in.available() == 0 && (System.currentTimeMillis() - start) < this.connectionTimeOut );
+
+                // Check if we received data or timed out
+                if (in.available() != 0)
+                {
+                    // Keep reading from input-stream as long as we have bytes to process.
+                    while (in.available() != 0)
+                    {
+                        // Read input-stream and store it in temporary buffer
+                        byte[] buf = new byte[buffSize];
+
+                        in.read(buf);
+                        temp.write(buf);
+                    }
+
+                    System.out.printf("HTTP request from %s using port %d \n", clientSocket.getInetAddress(), clientSocket.getPort());
+
+                    // For debugging purposes
+                    System.out.println(new String(temp.toByteArray(), "UTF-8"));
+
+                    String request = new String(temp.toByteArray(), "UTF-8").trim();
+
+                    /*
+                       Half dirty test below to test to send back a web-page to the client.
+                       Header currently static, copied from Wikipedias homepage
+                     */
+
+                    processRequest(request.getBytes(), out);
+
+                }
+                else
+                {
+                    done = true;
+                }
+            }
+
+            System.out.printf("Closing connection for %s on port %d \n", clientSocket.getInetAddress(), clientSocket.getPort());
+            this.clientSocket.close();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Help-method for sending back a response. This is just for testing purposes
+     * @param req
+     */
+    private void processRequest(byte[] req, OutputStream out) throws IOException {
+
+        // Make string-version of request
+        String request = new String(req, "UTF-8");
+
+        Scanner requestScanner = new Scanner(request);
+
+        // Grab the first word in the HTTP-request which is the method
+        String reqMeth = requestScanner.next();
+
+        String destinationFilePath;
+
+        // Atm GET is only supported
+        if (reqMeth.equals("GET"))
+        {
+            String fileRequested = requestScanner.next().substring(1);
+
+            // For this test, we only support index.html as a known file on the server
+            if (fileRequested.equals("index.html") || fileRequested.equals(""))
+            {
+                destinationFilePath = "http/resources/index.html";
+            }
+            else
+            {
+                destinationFilePath = "http/resources/fileNotFound.html";
+            }
+
+        }
+        else
+        {
+            destinationFilePath = "http/resources/fileNotFound.html";
+        }
+
+        String response = "";
+
+        /*
+        Form a response of a standard-header and the specific HTML-file
+         */
+
+        FileInputStream headerReader = new FileInputStream(new File("http/resources/standardHeader.txt"));
+        byte[] byteArr = new byte[headerReader.available()];
+        headerReader.read(byteArr);
+
+        response += new String(byteArr, "UTF-8");
+
+
+        FileInputStream dataFileReader = new FileInputStream(destinationFilePath);
+
+        byteArr = new byte[dataFileReader.available()];
+        dataFileReader.read(byteArr);
+
+        response += new String(byteArr, "UTF-8");
+
+        // For debugging purposes
+        System.out.println(response);
+
+        out.write(response.getBytes());
+
+    }
+
+}
+
+
--- src/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by peter on 2/14/17.
- */
-public class HelloWorld
-{
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
-}
